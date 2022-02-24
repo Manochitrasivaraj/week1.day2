b36e0f6a53204523b0b27bff6611697cc4b5db87
Manochitrasivaraj <100026601+Manochitrasivaraj@users.noreply.github.com>
2022-02-24 16:53:14 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calObj = new Calculator();
+		  calObj.add(5,2,4);
+		  calObj.substract(10,2);
+		  calObj.multiply(9,7);
+		  calObj.divide(5,2);
+		 
+	}
+
+}
