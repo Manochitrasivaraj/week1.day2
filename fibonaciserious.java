4d49558b7ba9252346082c6a8f0424caa342e57a
Manochitrasivaraj <100026601+Manochitrasivaraj@users.noreply.github.com>
2022-02-24 16:52:27 +0530

    Add files via upload

---
+++ fibonaciserious.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class fibonaciserious {
+
+	public static void main(String[] args) {
+		
+		
+		
+        int range = 8;
+        int i, firstNum = 0, secondNum = 1;
+        
+        int sum;
+  
+        for (i = 0; i < range; i++) {
+
+        	System.out.println(firstNum);
+            sum = firstNum+secondNum;
+            firstNum=secondNum;
+            secondNum=sum;
+            
+            
+ 
+		}
+
+		
+		}
+		
+
+	}
+
+
